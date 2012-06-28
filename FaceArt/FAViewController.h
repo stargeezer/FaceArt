7abd46d7280957fa02afd35bec5f015462ab2b1e
Mark Strand <stargezer@comcast.net>
2012-06-28 10:59:05 -0700

    added GLKViewController, testing git connection

---
+++ FaceArt/FAViewController.h
@@ -0,0 +1,13 @@
+//
+//  FAViewController.h
+//  FaceArt
+//
+//  Created by Mark Strand on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FAViewController : GLKViewController
+
+@end
---
+++ FaceArt/FAViewController.m
@@ -0,0 +1,13 @@
+//
+//  FAViewController.m
+//  FaceArt
+//
+//  Created by Mark Strand on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FAViewController.h"
+
+@implementation FAViewController
+
+@end
