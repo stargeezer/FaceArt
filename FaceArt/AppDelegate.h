9bb526c8758682e926d17660372405f95486a2b1
Mark Strand <stargezer@comcast.net>
2012-06-28 10:52:10 -0700

    Initial Commit

---
+++ FaceArt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FaceArt
+//
+//  Created by Mark Strand on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
